cb3dbeb1205a6e0f7dc5acd57d45c98b30bdd2ec
IceGuyMusic <50375955+IceGuyMusic@users.noreply.github.com>
2023-11-08 20:47:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def healthcheck():
     # Now you can access result.stdout and result.stderr
     output = result.stdout
     errors = result.stderr
-    results[file] = output
+    results[files] = output
 
     return jsonify(results)
 
